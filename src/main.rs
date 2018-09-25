afde32091f62f2bbcb447aec4bea232f21adbb42
akritrime <akritrime@gmail.com>
2018-09-25 12:20:13 +0530

    working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::iter::Peekable;
 use std::vec::IntoIter;
+// use std::collections::HashMap;
 
 // THE TOKENIZER
 // The first step of any compilation is breaking the input into chunks that can be fed into a parser. This is called lexical analysis.
@@ -91,24 +92,44 @@ fn tokenizer(input: &str) -> Result<Vec<Token>, String> {
 // ASTNode - the type that stores a token with assigned meaning
 #[derive(Debug)]
 enum ASTNode {
-    Program(Program),
+    Program(Vec<ASTNode>),
     NumberLiterals(Token),
     StringLiterals(Token),
-    CallExpression(Call),
+    CallExpression {
+        name: String,
+        params: Vec<ASTNode>
+    },
     EOF,
     Error(String)
 }
 
-#[derive(Debug)]
-struct Call {
-    name: String,
-    params: Vec<ASTNode>
+impl ASTNode {
+    fn is_eof(&self) -> bool {
+        match self {
+            ASTNode::EOF => true,
+            _ => false
+        }
+    }
+
+    // fn enter(&self, parent: &Option<&Self>) {
+
+    // }
+
+    // fn exit(&self, parent: &Option<&Self>) {
+        
+    // }
 }
 
-#[derive(Debug)]
-struct Program {
-    body: Vec<ASTNode>
-}
+// #[derive(Debug)]
+// struct Call {
+//     name: String,
+//     params: Vec<ASTNode>
+// }
+
+// #[derive(Debug)]
+// struct Program {
+//     body: Vec<ASTNode>
+// }
 
 fn parser(input: Vec<Token>) -> ASTNode {
     let mut current = 0;
@@ -122,33 +143,35 @@ fn parser(input: Vec<Token>) -> ASTNode {
             _ => return ASTNode::EOF
         };
 
+        // println!("{:?}", tokens.peek());
+
         match token {
             Token::Int(_) => ASTNode::NumberLiterals(token),
             Token::Str(_) => ASTNode::StringLiterals(token),
             Token::LParen => {
                 // skipping the LParen
                 *current += 1;
-                let mut ast_node = match tokens.next() {
-                    Some(Token::Name(name)) => Call {
-                        name,
-                        params: vec![]
-                    },
+                let mut name = match tokens.next() {
+                    Some(Token::Name(name)) => name,
 
                     _ => return ASTNode::Error(format!("Expected a function name at {}.", current))
                 };
                 
+                let mut params = vec![];
                 loop {
                     match tokens.peek() {
                         Some(Token::RParen) => break,
-                        Some(_) => (),
+                        Some(_) => params.push(walk(current, tokens)),
                         None => return ASTNode::Error(format!("Expected a ) at {}.", current))
                     }
-                    ast_node.params.push(walk(current, tokens))
                 }
 
                 tokens.next();
                 *current += 1;
-                ASTNode::CallExpression(ast_node)
+                ASTNode::CallExpression {
+                    name,
+                    params
+                }
 
             }
             _ => ASTNode::Error(format!("Unexpected {:?} at {}", token, current))
@@ -156,22 +179,119 @@ fn parser(input: Vec<Token>) -> ASTNode {
 
     }
 
-    let mut prog = Program {
-        body: vec![]
-    };
+    let mut body = vec![];
 
     loop {
-        if let None = tokens.peek() {
+        let ast_node = walk(&mut current, &mut tokens);
+        let end = ast_node.is_eof();
+        body.push(ast_node);
+        if end {
             break
-        };
-        prog.body.push(walk(&mut current, &mut tokens))
+        }
     }
 
-    ASTNode::Program(prog)
+    ASTNode::Program(body)
 }
 
+// THE TRAVERSER
+// The third step is traversing through the AST and visit all the nodes. 
+// struct Visitor {
+
+// }
+
+// fn traverser(ast: &ASTNode) {
+//     fn traverseArray(children: &Vec<ASTNode>, parent: &Option<&ASTNode>) {
+//         for child in children {
+//             traverseNode(child, parent)
+//         }
+//     }
+
+//     fn traverseNode(node: &ASTNode, parent: &Option<&ASTNode>) {
+//         node.enter(parent);
+
+//         match node {
+//             ASTNode::Program(body) => traverseArray(&body, &Some(node)),
+//             ASTNode::CallExpression { name: _, params } => traverseArray(&params, &Some(node)),
+//             _ => ()
+//         }
+
+//         node.exit(parent);
+//     }
+
+//     traverseNode(ast, &None)
+// }
+
+// THE TRANSFORMER
+// The transformer will take the AST that our parser generated, traverse the array and create a new AST.
+
+fn execute(node: ASTNode) -> Result<Vec<isize>, String> {
+
+    
+
+    match node {
+        ASTNode::Program(body) => {
+            let mut res = vec![];
+            for node in body {
+                let temp = execute(node);
+
+                match temp {
+                    Ok(n) => res.extend_from_slice(&n),
+                    _ => return temp
+                }
+            }
+
+            Ok(res)
+        },
+
+        ASTNode::CallExpression { name, params } => {
+            let mut params = params.into_iter().map(execute);
+            match name.as_str() {
+                "add" => {
+                    let mut res = 0;
+
+                    for param in params {
+                        match param {
+                            Ok(temp) => res += temp.first().unwrap_or(&0),
+                            _ => return param
+                        }
+                    }
+
+                    Ok(vec![res])
+                },
+
+                "subtract" => {
+                    let mut res = match params.next() {
+                        Some(temp) => match temp {
+                            Ok(temp) => *temp.first().unwrap_or(&0),
+                            _ => return temp
+                        },
+                        _ => return Ok(vec![0])
+                    };
+
+                    for param in params {
+                        match param {
+                            Ok(temp) => res -= temp.first().unwrap_or(&0),
+                            _ => return param
+                        }
+                    }
+
+                    Ok(vec![res])
+                },
+
+                _ => Err(format!("Not yet supported {}", name))
+            }
+        },
+
+        ASTNode::NumberLiterals(Token::Int(n)) => Ok(vec![n as isize]),
+        ASTNode::EOF => Ok(vec![]),
+        _ => Err(format!("Unexpected node: {:?}", node))
+    }
+}
 
 fn main() {
-    let tokens = tokenizer("(add 24 \"String\")").unwrap();
-    println!("{:?}", parser(tokens));
+    let tokens = tokenizer("(add 24 3 (subtract 3 1 5))").unwrap();
+    println!("{:?}", execute(parser(tokens)));
+    // for _ in 0..12 {
+    //     println!("{:?}", tokens.next());
+    // }
 }
